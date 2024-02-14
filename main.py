a4df1e249c29a5087f294e8dcb1b9b52c1b16da0
oschdez97 <56457304+oschdez97@users.noreply.github.com>
2024-02-14 20:43:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from dotenv import load_dotenv
 from src.runtime_client import LexRuntimeV2Client
 
 load_dotenv(".env")
-API_KEY = os.environ.get("API_KEY")
+API_KEY = os.environ.get("SECRETO")
 
 if __name__ == "__main__":
     print(os.environ)
     print(API_KEY)
+
+    with open(".env", "r") as f:
+        secret = f.readline().split("=")[1].strip()
+        print(secret)
