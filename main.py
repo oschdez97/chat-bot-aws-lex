1bbaabe9ed4fdb1bf03437ed62fd12e026d09039
oschdez97 <56457304+oschdez97@users.noreply.github.com>
2024-02-14 20:21:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import os, fire
 from dotenv import load_dotenv
 from src.runtime_client import LexRuntimeV2Client
 
-env_file = os.getenv('GITHUB_ENV')
-
 if __name__ == "__main__":
-    print(env_file)
+    print(os.environ)
