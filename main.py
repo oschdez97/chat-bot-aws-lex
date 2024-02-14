17143786cb9ec32c5b04f6e57a75ed3f46824c56
oschdez97 <56457304+oschdez97@users.noreply.github.com>
2024-02-14 20:31:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ import os, fire
 from dotenv import load_dotenv
 from src.runtime_client import LexRuntimeV2Client
 
+load_dotenv(".env")
+API_KEY = os.environ.get("API_KEY")
+
 if __name__ == "__main__":
     print(os.environ)
+    print(API_KEY)
